327066d48e2c980600c44dcbddae00cf18b1d82c
anas40 <manas1@myamu.ac.in>
2020-10-28 20:14:54 +0530

    maintain aspect ratio

--- src/components/reactCarousel.js
+++ src/components/reactCarousel.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 
+
+import c1 from '../../static/carousel/1.jpeg'
+import c2 from '../../static/carousel/2.jpeg'
+
+import c3 from '../../static/carousel/3.jpeg'
+import c4 from '../../static/carousel/4.jpeg'
+
+
 export default (props) => (
     <Carousel {...props}>
         <div>
-            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg" />
+            <img alt="" src={c1} />
             <p className="legend customizeLegend">Legend 1</p>
         </div>
         <div>
-            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" />
+            <img alt="" src={c2} />
             <p className="legend customizeLegend">Legend 2</p>
         </div>
         <div>
-            <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-3.jpg" />
+            <img alt="" src={c4} />
             <p className="legend customizeLegend">Legend 3</p>
         </div>     
     </Carousel>
