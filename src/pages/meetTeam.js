ec4abf170562263b640bee3838eec4108bf2980a
anas40 <manas1@myamu.ac.in>
2020-10-30 04:10:36 +0530

    meet team page

---
+++ src/pages/meetTeam.js
@@ -0,0 +1,54 @@
+import React from "react"
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import TitleHeading from '../components/TitleHeading'
+
+import '../css/meetTeam.scss'
+import PI from '../../static/president.webp'
+
+export default function Events() {
+
+    const members = [
+        { name: "Name", position: "Position",image:PI },
+        { name: "Name", position: "Position",image:PI },
+        { name: "Name", position: "Position",image:PI }
+    ]
+    return <div>
+        <Header />
+        <main>
+            <TitleHeading heading="Executive Team" body="Team which executes all functions." />
+            <section className="teamContainer">
+                <section className="onlyOne">
+                    <div className="card">
+                        <div className="imagePart">
+                            <img src={PI} alt="President of AMUAAS" />
+                        </div>
+                        <div className="textPart">
+                            <div>
+                                <h2>President</h2>
+                            </div>
+                            <p>APJ Abdul Kalam</p>
+                        </div>
+                    </div>
+                </section>
+                <section className="multiple">
+                    {members.map(person => <div className="card">
+                        <div className="imagePart">
+                            <img src={person.image} alt="President of AMUAAS" />
+                        </div>
+                        <div className="textPart">
+                            <div>
+                                <h2>{person.position}</h2>
+                            </div>
+                            <p>{person.name}</p>
+                        </div>
+                    </div>
+                    )
+                    }
+                </section>
+            </section>
+        </main>
+        <Footer />
+    </div>
+}
