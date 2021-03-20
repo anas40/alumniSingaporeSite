bccd5ac15eaba655b7bb9e27c8d4a8ff4b112f72
anas40 <manas1@myamu.ac.in>
2021-03-21 05:15:13 +0530

    header with grid

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ import logo from '../../static/logo.jpeg'
 
 //css
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
-import '../css/header.css'
+import '../css/header.scss'
 
 //components
 import { Navbar, Nav } from 'react-bootstrap'
@@ -24,38 +24,33 @@ import contactContent from "../../static/store/contact"
 function Header() {
     return <header>
         <section>
-            <div className="left">
-                <div id="logo"><img src={logo} alt="logo" /></div>
-                <div className="Hcontainer">
-                    <div className="associationName">
-                        <p>AMU Alumni Association</p>
-                    </div>
-                    <div className="location">
-                        <p>Singapore</p>
-                    </div>
-                    <div className="nonProfit">
-                        <p>AMUAAS aims to keep alive Sir Syed Ahmad Khan's dream of ensuring equality
+            <div id="logo"><img src={logo} alt="logo" /></div>
+            <div className="Hcontainer">
+                <div className="associationName">
+                    <p>AMU Alumni Association</p>
+                </div>
+                <div className="location">
+                    <p>Singapore</p>
+                </div>
+                <div className="nonProfit">
+                    <p>AMUAAS aims to keep alive Sir Syed Ahmad Khan's dream of ensuring equality
             for all.</p>
-                    </div>
                 </div>
             </div>
-            <div className="right">
-                <div className="contact">
-                    <div><a href={`mailto:${contactContent.email}`}><MdEmail />{contactContent.email}</a></div>
-                    <div><a href={`tel:${contactContent.number}`}><FaPhone />{contactContent.number}</a></div>
+
+            <div className="contact">
+                <div><a href={`mailto:${contactContent.email}`}><MdEmail />{contactContent.email}</a></div>
+                <div><a href={`tel:${contactContent.number}`}><FaPhone />{contactContent.number}</a></div>
+            </div>
+            <div className="reach">
+                <div className="links">
+                    <div><a href={contactContent.facebook} target="_blank"><FaFacebook /></a></div>
                 </div>
-                <div className="reach">
-                    <div className="links">
-                        <div><a href={contactContent.facebook} target="_blank"><FaFacebook /></a></div>
-                        {/* <div><a href={contactContent.twitter}><FaTwitter /></a></div>
-                        <div><a href={contactContent.instagram}><FaInstagram /></a></div>
-                        <div><a href={contactContent.youtube}><FaYoutube /></a></div> */}
-                    </div>
-                    <Button target="_blank" href="https://www.cognitoforms.com/AMUAlumniAssociationSingapore/AMUAlumniAssociationSingaporeRegistration" className="donateButton" variant="contained" color="secondary">
-                        Join Us
+                <Button target="_blank" href="https://www.cognitoforms.com/AMUAlumniAssociationSingapore/AMUAlumniAssociationSingaporeRegistration" className="donateButton" variant="contained" color="secondary">
+                    Join Us
                     </Button>
-                </div>
             </div>
+
         </section>
         <NavCompo />
     </header>
